ea2ae44a76aa1f3ae3d5bab84d941f414ac28592
Rodrigo García <yo@rodrigogarcia.me>
2020-02-16 12:12:05 +0100

    Update specialization-insects

--- packages/specialization-insects/paint-worklet.js
+++ packages/specialization-insects/paint-worklet.js
@@ -1,34 +1,25 @@
 /* eslint-disable */
 
-import { drawHorizontalLine, drawVerticalLine } from '../shared/draw.js';
-import { range } from '../shared/utils.js';
+const BORDER_SIZE = 4;
 
-const xStart = 10;
-const xStep = 20;
-const xSpread = 10;
-const yInnerPadding = 10;
-const yOuterPadding = 20;
-const yPaddingStep = 2;
-const yStart = 10;
-const yStep = 20;
-const ySpread = 10;
-
-class ContainerBackgroundPainter {
+class ArticlePainter {
   paint(ctx, { height, width }) {
-    const xStop = width - xStart;
-    const yStop = height - yStart;
+    ctx.save();
 
-    for (const y of range(yInnerPadding, yOuterPadding, yPaddingStep)) {
-      drawHorizontalLine(ctx, xStart, xStop, xStep, y, ySpread);
-    }
+    const gradient = ctx.createLinearGradient(0, 0, 1, height);
+    gradient.addColorStop(0, 'white');
+    gradient.addColorStop(0.5, '#888888');
+    gradient.addColorStop(1, '#444444');
 
-    for (const y of range(height - yOuterPadding, height - yInnerPadding, yPaddingStep)) {
-      drawHorizontalLine(ctx, xStart, xStop, xStep, y, ySpread);
-    }
+    ctx.fillStyle = gradient;
 
-    drawVerticalLine(ctx, yStart, yStop, yStep, xStart, xSpread);
-    drawVerticalLine(ctx, yStart, yStop, yStep, xStop, xSpread);
+    ctx.fillRect(0, 0, width, BORDER_SIZE); // top
+    ctx.fillRect(width - BORDER_SIZE, 0, BORDER_SIZE, height); // right
+    ctx.fillRect(0, height - BORDER_SIZE, width, BORDER_SIZE); // bottom
+    ctx.fillRect(0, 0, BORDER_SIZE, height); // left
+
+    ctx.restore();
   }
 }
 
-registerPaint('container-background', ContainerBackgroundPainter);
+registerPaint('article', ArticlePainter);
