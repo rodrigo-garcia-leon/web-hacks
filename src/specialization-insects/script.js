0515098e7a3fac057d4d2a671e6761dc32b1ddaa
Rodrigo García <yo@rodrigogarcia.me>
2022-03-01 14:59:09 +0100

    Refactor project structure

--- src/specialization-insects/script.js
+++ src/specialization-insects/script.js
@@ -1,7 +1,7 @@
 async function installPaintWorklet() {
   if ('paintWorklet' in CSS) {
     try {
-      await CSS.paintWorklet.addModule('/packages/specialization-insects/paint-worklet.js');
+      await CSS.paintWorklet.addModule('/specialization-insects/paint-worklet.js');
     } catch (error) {
       console.error(error); // eslint-disable-line
     }
