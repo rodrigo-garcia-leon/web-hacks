b792f0078e402366c5011ad60c0354cf3e37129b
Rodrigo García <yo@rodrigogarcia.me>
2022-03-01 18:07:28 +0100

    Implement jobs-explorer render

--- src/jobs-explorer/script.js
+++ src/jobs-explorer/script.js
@@ -19,11 +19,30 @@ class JobsExplorer extends HTMLElement {
 
   render() {
     this.innerHTML = `<main>
-    <h1>Jobs Explorer</h1>
-    <div id="jobs">
-        <h2>Jobs</h2>
-        <p>${this.jobs}</p>
+  <h1>Jobs Explorer</h1>
+  <section id="jobs">
+    <h2>Jobs</h2>
+    <div id="jobs-container">
+      ${this.jobs
+        .reverse()
+        .map(
+          job => `<div class="job">
+<h3><a href="${job.url}" target="_blank" rel="noopener noreferrer">${job.position}</a></h3>
+<p class="date">${job.modified}</p>
+<p class="company">${job.company}</p>
+<p class="location">${job.location}</p>
+${
+  job.keywords.length
+    ? `<ul class="keywords">
+${job.keywords.map(keyword => `<li>${keyword}</li>`).join('')}
+<ul>`
+    : ''
+}
+</div>`,
+        )
+        .join('')}
     </div>
+  </section>
 </main>`;
   }
 }
